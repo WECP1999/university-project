b8f9190833c8fd9ffcd87891c81030696b0f57e6
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-12 16:14:23 -0600

    :sparkles: add access screens

---
+++ screens/logInScreen/LogInScreen.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from '../../components/Themed';
+
+const LogInScreen = () => {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+};
+export default LogInScreen;
