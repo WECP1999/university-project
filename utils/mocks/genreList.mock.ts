842ca7760875204c422ef921fa95302208db872f
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-14 14:20:36 -0600

    :sparkles: add mocks for genre and manga

---
+++ utils/mocks/genreList.mock.ts
@@ -0,0 +1,66 @@
+import IGenre from '../interfaces/IGenre';
+
+const genreList: readonly IGenre[] = Object.freeze([
+  {
+    id: 1,
+    name: 'Action',
+    description: '',
+  },
+  {
+    id: 2,
+    name: 'Adventure',
+    description: '',
+  },
+  {
+    id: 3,
+    name: 'Comedy',
+    description: '',
+  },
+  {
+    id: 4,
+    name: 'Drama',
+    description: '',
+  },
+  {
+    id: 5,
+    name: 'Slice Of Life',
+    description: '',
+  },
+  {
+    id: 6,
+    name: 'Fantasy',
+    description: '',
+  },
+  {
+    id: 7,
+    name: 'Magic',
+    description: '',
+  },
+  {
+    id: 8,
+    name: 'Supernatural',
+    description: '',
+  },
+  {
+    id: 9,
+    name: 'Horror',
+    description: '',
+  },
+  {
+    id: 10,
+    name: 'Mystery',
+    description: '',
+  },
+  {
+    id: 11,
+    name: 'Psychological',
+    description: '',
+  },
+  {
+    id: 12,
+    name: 'Romance',
+    description: '',
+  },
+]);
+
+export default genreList;
