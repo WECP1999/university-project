a7f6b8b6b557c7c4945e2b0d040fd1277a151e13
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-11-04 12:05:44 -0600

    :sparkles: add firebase config and default app values

---
+++ firebase-config.ts
@@ -0,0 +1,16 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDunceO-flIyMix5BGeTDASwerlXQv8hJc',
+  authDomain: 'ichiran-3732d.firebaseapp.com',
+  projectId: 'ichiran-3732d',
+  storageBucket: 'ichiran-3732d.appspot.com',
+  messagingSenderId: '1058473843861',
+  appId: '1:1058473843861:web:3475eae1143887c52e8f9c',
+  measurementId: 'G-DCZ85ZN97D',
+};
+
+export default firebaseConfig;
