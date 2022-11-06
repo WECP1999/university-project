f37a4ccddb59bc0b916c5502d3421780f35f5e3f
WECP1999 <wecp123@gmail.com>
2022-11-06 00:15:06 -0600

    feat: Search Screen finished

--- screens/filterScreen/FilterScreen.tsx
+++ screens/filterScreen/FilterScreen.tsx
@@ -1,35 +1,86 @@
-import React from 'react'
+import { useState, useContext, useEffect } from "react";
 import { Text, View } from '../../components/Themed';
-import { StyleSheet, Pressable } from 'react-native';
+import { StyleSheet, Pressable, ScrollView, Image } from 'react-native';
 import Icon from '../../components/icon/Icon';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../utils/types/types';
-import { FormProvider, useForm } from 'react-hook-form';
-import CustomInput from '../../components/customInput';
+import { Input } from '@ui-kitten/components';
+
+import ItemContext from '../../context/provider/ItemProvider';
+import ItemPreview from '../../components/itemPreview';
+import IGenericItem from '../../utils/interfaces/IGenericItem';
+
 
 type SearchScreenType = NativeStackScreenProps<RootStackParamList, 'Search'>;
 
-const FilterScreen = ({ route, navigation }: SearchScreenType) => {
-    const methods = useForm();
+const FilterScreen = ({ navigation }: SearchScreenType) => {
+    const [searchItem, setSearchItem] = useState("");
+
+    const [listToDisplay, setListToDisplay] = useState<IGenericItem[]>([]);
+
+    const {
+        state: { items },
+    } = useContext(ItemContext);
+
+
+    const handleChange = (e: string) => {
+        setSearchItem(e);
+      };
+
+      let counter = 0;
+     
+    useEffect(() => {
+        if ( items && (searchItem !== "" || counter > 0)) {
+            counter +=1;
+            let list = items.filter((item) => {
+               return item.name.includes(searchItem);
+             });
+             setListToDisplay(list);
+         }else{
+            setListToDisplay([]);
+
+         }
+    }, [searchItem]);
+
+      
   return (
-    <View>
+    <View style={styles.container}>
       <View style={styles.headerNav}>
         <Pressable onPress={() => navigation.goBack()}>
             <Icon size={24} name="arrow-left" color="" />
         </Pressable>
-        <FormProvider  {...methods}>
-            <CustomInput
+            <Input
                 textStyle={{color: 'black'}}
                 size="large"
                 style={styles.input}
                 label=""
-                name="search"
+                
+                onChangeText={(value) => handleChange(value)}
                 placeholder="Search"
                 accessoryRight={() => {
                     return <Icon size={24} name="search" color="black" />;
             }}/>
-        </FormProvider>
       </View>
+      <ScrollView>
+      <View style={styles.containerResult}>
+        {listToDisplay.length >0 ?
+          listToDisplay.map((manga, index) => (
+            <ItemPreview
+              key={manga.id}
+              item={manga}
+              style={{
+                marginLeft: (index + 1) % 2 === 0 ? 36 : 0,
+              }}
+            />
+          )) : 
+            <View style={styles.noResultsContainer}>
+                <Image 
+                style={styles.noResultImage}
+                    source={require('../../assets/images/message.png')} />
+                <Text style={styles.noResults}>There is not data found</Text>
+            </View>}
+      </View>
+    </ScrollView>
     </View>
   )
 }
@@ -46,5 +97,35 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#ffff',
         borderColor: 'transparent'
+    },
+    containerResult: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'flex-start',
+        width: '100%',
       },
+    container: {
+        padding: 8,
+        backgroundColor: '#fff',
+        width: '100%',
+        flex:1
+    },
+    noResultsContainer:{
+        flex:1,
+        justifyContent:'center',
+        marginVertical:150,
+    },
+    noResults:{
+        textAlign: 'center',
+        fontSize: 16,
+        marginVertical:20
+    },
+    noResultImage: {
+        display: 'flex',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+    }
   });
+
+  
+
